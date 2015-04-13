35b57460ac21a4d87ccd3f56f05096bcff60c822
Emily Berkeley <eberkeley@gmail.com>
2015-04-13 08:24:44 -0600

    TriangleError not being raised

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  unless all_sides_positive(a, b, c)
+    raise TriangleError
+  end
+
   if (a == b && a == c)
     :equilateral
   elsif (a == b || a == c || b == c)
@@ -21,6 +25,20 @@ def triangle(a, b, c)
   else
     :scalene
   end
+end
+
+def all_sides_positive(a, b, c)
+  if(a > 0 && b > 0 && c > 0)
+    valid_triangle(a, b, c)
+  else
+    false
+  end
+end
+
+def valid_triangle(a, b, c)
+  sides = [a, b, c].sort
+  longest = sides[2]
+  longest > sides[0] + sides[1]
 end
 
 # Error class used in part 2.  No need to change this code.
